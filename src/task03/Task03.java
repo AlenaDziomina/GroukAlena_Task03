bd5ca422a65ef97eb49d5030ea2130914fc2470b
Alena <groukhelena@gmail.com>
2014-08-12 11:17:27 +0200

    start

---
+++ src/task03/Task03.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package task03;
+
+/**
+ *
+ * @author Helena.Grouk
+ */
+public class Task03 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
